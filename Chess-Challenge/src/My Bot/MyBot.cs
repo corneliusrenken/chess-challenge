8e32ac6afe7f91d122ff6574c8884f6021711406
Cornelius Renken <cornelius.renken@gmail.com>
2023-07-23 14:12:07 +0200

    AapBot_V1 +59=92-3

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,145 @@
-﻿using ChessChallenge.API;
+﻿// AapBot_V1 +59=92-3
 
-public class MyBot : IChessBot
-{
-    public Move Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+using ChessChallenge.API;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MyBot : IChessBot {
+  private static Dictionary<PieceType, int> pieceValues = new Dictionary<PieceType, int>() {
+    {PieceType.Pawn, 100},
+    {PieceType.Knight, 305},
+    {PieceType.Bishop, 333},
+    {PieceType.Rook, 563},
+    {PieceType.Queen, 950}
+  };
+
+  private static float GetSideScore(Board board, bool white) {
+    int pieceScore = 0;
+    List<int> distances = new List<int>();
+
+    foreach (PieceType type in pieceValues.Keys) {
+      PieceList list = board.GetPieceList(type, white);
+      int pieceValue = pieceValues[type];
+      pieceScore += pieceValue * list.Count;
+
+      for (int i = 0; i < list.Count; i += 1) {
+        Piece piece = list[i];
+        distances.Add(white ? piece.Square.Rank : 7 - piece.Square.Rank);
+      }
     }
+
+    float positionScore = distances.Count == 0 ? 0 : (float)(distances.Average() / 7) * 50;
+    return pieceScore + positionScore;
+  }
+
+  private static float GetBoardScore(Board board, bool white) {
+    return GetSideScore(board, white) - GetSideScore(board, !white);
+  }
+
+  private static Move? GetBestMove(Board board) {
+    Move[] moves = board.GetLegalMoves();
+
+    if (moves.Length == 0) {
+      return null;
+    }
+
+    float bestScore = float.MinValue;
+    Move bestMove = moves[0];
+    bool white = board.IsWhiteToMove;
+
+    foreach (Move move in moves) {
+      board.MakeMove(move);
+      float score = GetBoardScore(board, white);
+      board.UndoMove(move);
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = move;
+      }
+    }
+
+    return bestMove;
+  }
+
+  public static Move GetBestMoveWithForesight(Board board) {
+    Move[] moves = board.GetLegalMoves();
+    Move?[] rebukes = new Move?[moves.Length];
+    for (int i = 0; i < moves.Length; i += 1) {
+      Move move = moves[i];
+      board.MakeMove(move);
+      Move? opponentsBestMove = GetBestMove(board);
+      board.UndoMove(move);
+
+      rebukes[i] = opponentsBestMove;
+    }
+
+    float bestScore = float.MinValue;
+    Move bestMove = moves[0];
+
+    for (int i = 0; i < moves.Length; i += 1) {
+      Move move = moves[i];
+      Move? opponentsBestMove = rebukes[i];
+
+      // if opponent can't rebuke, it's a winning move I think
+      if (opponentsBestMove == null) {
+        return move;
+      }
+
+      board.MakeMove(move);
+      board.MakeMove((Move)opponentsBestMove);
+      float score = GetBoardScore(board, board.IsWhiteToMove);
+      board.UndoMove((Move)opponentsBestMove);
+      board.UndoMove(move);
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = move;
+      }
+    }
+
+    return bestMove;
+  }
+
+  public Move Think(Board board, Timer timer) {
+    bool white = board.IsWhiteToMove;
+    // get current best move for opponent
+    bool skipped = board.TrySkipTurn();
+    if (skipped) {
+      Move? opponentsBestMove = GetBestMove(board);
+      board.UndoSkipTurn();
+
+      if (opponentsBestMove != null) {
+        bool opponentWouldCapture = ((Move)opponentsBestMove).CapturePieceType != PieceType.None;
+        if (opponentWouldCapture) {
+          Piece underThreat = board.GetPiece(((Move)opponentsBestMove).TargetSquare);
+          // get all squares that the piece can move to
+          Move[] moves = board.GetLegalMoves();
+
+          float bestScore = float.MinValue;
+          Move? bestMove = null;
+
+          foreach (Move move in moves) {
+            if (move.StartSquare == underThreat.Square) {
+              board.MakeMove(move);
+              float score = GetBoardScore(board, white);
+              board.UndoMove(move);
+
+              if (score > bestScore) {
+                bestScore = score;
+                bestMove = move;
+              }
+            }
+          }
+
+          if (bestMove != null) {
+            return (Move)bestMove;
+          }
+        }
+      }
+    }
+
+    return GetBestMoveWithForesight(board);
+  }
 }
